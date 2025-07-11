a8c532ca5d3e5a71804500ebd7646ebaf58084fd
Steve Ryan <steve.ryan@asu.edu>
2025-07-11 14:55:56 -0700

    Add block binding for excerpt

--- inc/block-bindings.php
+++ inc/block-bindings.php
@@ -25,6 +25,12 @@ function pitchfork_engnews_register_block_bindings() {
 		'uses_context' => array( 'postId' ),
 	) );
 
+	register_block_bindings_source( 'engnews/queried-story-excerpt', array(
+		'label'              => __( 'Queried permalink', 'pitchfork_engnews' ),
+		'get_value_callback' => 'pitchfork_engnews_loop_excerpt_binding',
+		'uses_context' => array( 'postId' ),
+	) );
+
 }
 
 /**
@@ -58,9 +64,56 @@ function pitchfork_engnews_loop_permalink_binding() {
         return '';
     }
 
-	// if ( ! empty( $block_context['postId'] ) ) {
-	// 	return get_permalink( $block_context['postId'] );
-	// }
-
 	return get_permalink($post_id);
+}
+
+/**
+ * Return the permalink of a queried post in a post loop.
+ */
+
+function pitchfork_engnews_loop_excerpt_binding() {
+
+	$post_id = get_the_ID();
+    if (!$post_id) {
+        return '';
+    }
+
+	return get_the_excerpt($post_id);
+}
+
+
+/**
+ * Retrieves a limited number of direct paragraph blocks from within the
+ * `acf/news-article-grid` block of a given post.
+ *
+ * @param int $post_id The ID of the post to extract content from.
+ * @param int $limit   Optional. Maximum number of paragraph blocks to return. Default 10.
+ * @return string[]    Array of rendered HTML paragraph strings.
+ */
+function pitchfork_engnews_get_post_content_preview( $post_id, $limit = 10 ) {
+
+	$post = get_post( $post_id );
+
+	if ( ! $post ) {
+		return array();
+	}
+
+	$blocks = parse_blocks( $post->post_content );
+	$paragraphs = array();
+
+	foreach ( $blocks as $block ) {
+		if ( $block['blockName'] === 'acf/news-article-grid' && ! empty( $block['innerBlocks'] ) ) {
+			foreach ( $block['innerBlocks'] as $child_block ) {
+				if ( $child_block['blockName'] === 'core/paragraph' ) {
+					$paragraphs[] = apply_filters( 'the_content', render_block( $child_block ) );
+
+					if ( count( $paragraphs ) >= $limit ) {
+						break 2; // Exit both loops once limit is hit
+					}
+				}
+			}
+		}
+	}
+
+	return $paragraphs;
 }
